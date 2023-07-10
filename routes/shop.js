2529c3e6042bc62c6154deddcd61809afa9fc4ab
rajat0017 <rajatkalgaonkar07@gamil.com>
2023-07-10 13:51:40 +0530

    controllers

--- routes/shop.js
+++ routes/shop.js
@@ -5,18 +5,10 @@ const express = require('express');
 const rootDir = require('../util/path');
 const adminData = require('./admin');
 
+const productcontroller= require('../controllers/products')
+
 const router = express.Router();
 
-router.get('/', (req, res, next) => {
-  const products = adminData.products;
-  res.render('shop', {
-    prods: products,
-    pageTitle: 'Shop',
-    path: '/',
-    hasProducts: products.length > 0,
-    activeShop: true,
-    productCSS: true
-  });
-});
+router.get('/', productcontroller.getProducts);
 
 module.exports = router;
