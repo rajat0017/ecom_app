514160a1a21ec4b952fd552c39c4ebdc1605dbed
rajat0017 <rajatkalgaonkar07@gamil.com>
2023-07-17 13:02:20 +0530

    sequelize

--- util/database.js
+++ util/database.js
@@ -1,10 +1,8 @@
-const mysql = require('mysql2');
+const Sequelize = require('sequelize');
 
-const pool = mysql.createPool({
-   host:'localhost',
-   user:'root',
-   database:'nodejs',
-   password:'Rajat@7208'
+const sequelize = new Sequelize('nodejs','root','Rajat@7208',{
+    dialect : 'mysql',
+    host:'localhost'
 });
 
-module.exports= pool.promise();
+module.exports = sequelize;
