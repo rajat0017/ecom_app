1c474cf3e99450a85ca82eae8ffccf69b08fc481
rajat0017 <rajatkalgaonkar07@gamil.com>
2023-07-24 22:17:10 +0530

    User and Product relationship

--- controllers/admin.js
+++ controllers/admin.js
@@ -13,13 +13,19 @@ exports.postAddProduct = (req, res, next) => {
   const imageUrl = req.body.imageUrl;
   const price = req.body.price;
   const description = req.body.description;
-  const product = new Product(null, title, imageUrl, description, price);
-  product
-    .save()
-    .then(() => {
-      res.redirect('/');
-    })
-    .catch(err => console.log(err));
+  req.user.createProduct({
+    title:title,
+    price:price,
+    imageUrl:imageUrl,
+    description:description
+  })
+  .then(result=>{
+    console.log('created product');
+    res.redirect('/admin/products')
+  })
+  .catch(err=>{
+    console.log(err);
+  });
 };
 
 exports.getEditProduct = (req, res, next) => {
@@ -28,7 +34,10 @@ exports.getEditProduct = (req, res, next) => {
     return res.redirect('/');
   }
   const prodId = req.params.productId;
-  Product.findById(prodId, product => {
+  req.user.getProducts({where:{id:prodId}})
+  // Product.findByPk(prodId)
+  .then( products => {
+    const product = products[0];
     if (!product) {
       return res.redirect('/');
     }
@@ -38,6 +47,9 @@ exports.getEditProduct = (req, res, next) => {
       editing: editMode,
       product: product
     });
+  })
+  .catch(err=>{
+    console.log(err);
   });
 };
 
@@ -47,29 +59,48 @@ exports.postEditProduct = (req, res, next) => {
   const updatedPrice = req.body.price;
   const updatedImageUrl = req.body.imageUrl;
   const updatedDesc = req.body.description;
-  const updatedProduct = new Product(
-    prodId,
-    updatedTitle,
-    updatedImageUrl,
-    updatedDesc,
-    updatedPrice
-  );
-  updatedProduct.save();
-  res.redirect('/admin/products');
+  Product.findByPk(prodId)
+  .then(product=>{
+    product.title=updatedTitle;
+    product.price=updatedPrice;
+    product.description=updatedDesc;
+    product.imageUrl=updatedImageUrl;
+    return product.save();
+  })
+  .then(result=>{
+    console.log('UPDATED PRODUCT');
+    res.redirect('/admin/products');
+  })
+  .catch(err=>{
+    console.log(err);
+  });
 };
 
 exports.getProducts = (req, res, next) => {
-  Product.fetchAll(products => {
+  req.user.getProducts()
+  .then(products => {
     res.render('admin/products', {
       prods: products,
       pageTitle: 'Admin Products',
       path: '/admin/products'
     });
+  })
+  .catch(err=>{
+    console.log(err);
   });
 };
 
 exports.postDeleteProduct = (req, res, next) => {
   const prodId = req.body.productId;
-  Product.deleteById(prodId);
-  res.redirect('/admin/products');
+  Product.findByPk(prodId)
+  .then(product=>{
+    return product.destroy();
+  })
+  .then(result=>{
+    console.log('DESTROYED PRODUCT');
+    res.redirect('/admin/products');
+})
+  .catch(err =>{
+    console.log(err);
+  });
 };
--- util/database.js
+++ util/database.js
@@ -1,8 +1,8 @@
 const Sequelize = require('sequelize');
 
-const sequelize = new Sequelize('nodejs','root','Rajat@7208',{
-    dialect : 'mysql',
+const sequelize = new Sequelize('node-complete','root','Rajat@7208',{
+    dialect:'mysql',
     host:'localhost'
 });
 
-module.exports = sequelize;
+module.exports=sequelize;
